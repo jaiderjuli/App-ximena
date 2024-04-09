8f3e8a24e74cd6d91c770c91d32f2f8962080807
julian rodriguez <jaiderjulianr@gmail.com>
2024-04-08 22:57:27 -0500

    enviar archivos a repo

---
+++ index.js
@@ -0,0 +1,18 @@
+const buttonSi = document.getElementById('yes');
+const buttonNo = document.getElementById('no');
+
+const Lo_sabía = (event) => {
+  event.preventDefault(); // Evita que la página se recargue al hacer clic
+  alert('NOS ALEGRA QUE NOS PUEDAS ACOMPAÑAR😊 (ya sabe que está comprometido con Dios🫵)');
+  alert('🤩a las 6:00pm te esperamos😎');
+  location.href = 'https://www.youtube.com/watch?v=QKD2Bb17ONA=1:54s';
+};
+
+const func_event = () => {
+  buttonNo.style.position = 'absolute';
+  buttonNo.style.top = (Math.random() * window.innerHeight) + 'px';
+  buttonNo.style.left = (Math.random() * window.innerWidth) + 'px';
+};
+
+buttonSi.addEventListener('click', Lo_sabía);
+buttonNo.addEventListener('mouseover', func_event);
