fea7c8bc898bdee954b5194c019ed4850dce0409
julian rodriguez <jaiderjulianr@gmail.com>
2024-04-12 10:28:16 -0500

    findeman

--- script.js
+++ script.js
@@ -1,29 +1,20 @@
+const buttonSi = document.getElementById('yes');
+const buttonNo = document.getElementById('no');
 
-const listaNombres = document.getElementById('listaNombres');
-let nombresSi = [];
-
-const Lo_sabía = () => {
-  const nombre = prompt("Por favor, ingresa tu nombre:");
-  if (nombre) {
-    nombresSi.push(nombre);
-    actualizarLista();
-    alert(`Hola, ${nombre}! NOS ALEGRA QUE NOS PUEDAS ACOMPAÑAR😊 (ya sabe que está comprometido con Dios🫵)`);
-    alert('🤩a las 6:00pm te esperamos😎 app-ximena');
-    location.href = 'https://www.youtube.com/watch?v=QKD2Bb17ONA=1:54s';
-  } else {
-    alert("Por favor, ingresa un nombre válido.");
-  }
+const Lo_sabía = (event) => {
+  event.preventDefault(); // Evita que la página se recargue al hacer clic
+  alert('NOS ALEGRA QUE NOS PUEDAS ACOMPAÑAR😊 (ya sabe que está comprometido con Dios🫵)');
+  alert('🤩a las 6:00pm te esperamos😎');
+  location.href = 'https://www.youtube.com/watch?v=QKD2Bb17ONA=1:54s';
 };
 
-
-
-const app_xime = () => {
+const xime_app = () => {
   buttonNo.style.position = 'absolute';
   buttonNo.style.top = (Math.random() * window.innerHeight) + 'px';
   buttonNo.style.left = (Math.random() * window.innerWidth) + 'px';
 };
 
 buttonSi.addEventListener('click', Lo_sabía);
-buttonNo.addEventListener('mouseover', app_xime);
+buttonNo.addEventListener('mouseover', xime_app);
 
 
